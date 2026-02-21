6cbe5f386c5d93e533ec59e89497029ce283eced
SerhiyKoval <kovalss2006@gmail.com>
2026-02-21 12:02:13 +0200

    Update RunTask

--- src/RunTask.java
+++ src/RunTask.java
@@ -7,11 +7,8 @@ public class RunTask {
     private static final Long SEED = 42L;
     private static final int MIN_ELEM = 0;
     private static final int MAX_ELEM = 10000000;
-    private final ArrayList<int[][]> matrices = new ArrayList<>();
+    private static final int RUNS = 5;
     public RunTask() {
-        for (int n : N) {
-            matrices.add(MatrixUtils.generateTestMatrix(n, n, MIN_ELEM, MAX_ELEM, SEED));
-        }
     }
     private Result multiplesSequentially(int[][] matrix){
         int count = 0;
@@ -28,7 +25,6 @@ public class RunTask {
         }
         return new Result(count, maxMultiple);
     }
-
     private void multiplesWithThreads(int[][] matrix, int threadCount, Accumulator accumulator) {
         int n = matrix.length;
         Thread[] threads = new Thread[threadCount];
@@ -45,7 +41,6 @@ public class RunTask {
             }
         }
     }
-
     private void createThreadTask(int[][] matrix, int threadIndex, int threadCount, Accumulator accumulator) {
         int n = matrix.length;
         for (int i = threadIndex; i < n; i+=threadCount) {
@@ -56,32 +51,61 @@ public class RunTask {
             }
         }
     }
+    private void warmUp() {
+        System.out.println("--- JVM warm-up start ---");
+        int[][] dummyMatrix = MatrixUtils.generateTestMatrix(5000, 5000, MIN_ELEM, MAX_ELEM, SEED);
+        for (int i = 0; i < 5; i++) {
+            multiplesSequentially(dummyMatrix);
+            multiplesWithThreads(dummyMatrix, 4, new SynchronizedAccumulator());
+            multiplesWithThreads(dummyMatrix, 4, new AtomicAccumulator());
+        }
+        dummyMatrix = null;
+        System.gc();
+        System.out.println("--- Warming up is complete. JVM optimized. ---\n");
+    }
     public void run() {
+        warmUp();
         for (int n : N) {
             System.out.println("===========================================");
-            System.out.println("Розмір матриці: " + n + "x" + n);
+            System.out.println("Matrix size: " + n + "x" + n);
 
             int[][] matrix = MatrixUtils.generateTestMatrix(n, n, MIN_ELEM, MAX_ELEM, SEED);
+            long totalTime = 0;
+            long startTime, endTime;
+            Result resultSeq = null;
+            for (int i = 0; i < RUNS; i++) {
+                startTime = System.nanoTime();
+                resultSeq = multiplesSequentially(matrix);
+                endTime = System.nanoTime();
+                totalTime += (endTime - startTime);
+            }
+            System.out.println("[Sequential] " + resultSeq + ", Time: " + totalTime/1_000_000/RUNS + " ms");
 
-            long startTime = System.currentTimeMillis();
-            Result resultSeq = multiplesSequentially(matrix);
-            long endTime = System.currentTimeMillis();
-            System.out.println("[Sequential] " + resultSeq + ", Time: " + (endTime - startTime) + " ms");
 
             for (int threads : THREAD_COUNTS) {
-                System.out.println("--- Потоків: " + threads + " ---");
+                System.out.println("--- Threads: " + threads + " ---");
 
-                Accumulator syncAcc = new SynchronizedAccumulator();
-                startTime = System.currentTimeMillis();
-                multiplesWithThreads(matrix, threads, syncAcc);
-                endTime = System.currentTimeMillis();
-                System.out.println("[Synchronized] " + syncAcc.getFinalResult() + ", Time: " + (endTime - startTime) + " ms");
+                Accumulator syncAcc = null;
+                totalTime = 0;
+                for (int i = 0; i < RUNS; i++) {
+                    syncAcc = new SynchronizedAccumulator();
+                    startTime = System.nanoTime();
+                    multiplesWithThreads(matrix, threads, syncAcc);
+                    endTime = System.nanoTime();
+                    totalTime += (endTime - startTime);
+                }
+                System.out.println("[Synchronized] " + syncAcc.getFinalResult() + ", Time: " + totalTime/1_000_000/RUNS + " ms");
 
-                Accumulator atomicAcc = new AtomicAccumulator();
-                startTime = System.currentTimeMillis();
-                multiplesWithThreads(matrix, threads, atomicAcc);
-                endTime = System.currentTimeMillis();
-                System.out.println("[Atomic CAS]   " + atomicAcc.getFinalResult() + ", Time: " + (endTime - startTime) + " ms");
+                Accumulator atomicAcc = null;
+                totalTime = 0;
+                for (int i = 0; i < RUNS; i++) {
+                    atomicAcc = new AtomicAccumulator();
+                    startTime = System.nanoTime();
+                    multiplesWithThreads(matrix, threads, atomicAcc);
+                    endTime = System.nanoTime();
+                    totalTime += (endTime - startTime);
+                }
+                System.out.println("[Atomic CAS]   " + atomicAcc.getFinalResult() + ", Time: " + totalTime/1_000_000/RUNS + " ms");
             }
 
             matrix = null;
